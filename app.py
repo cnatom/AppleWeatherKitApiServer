5cdcb3d23742616c7914388cc1c9692b0decc717
CUMT-Atom <cnatom@foxmail.com>
2022-07-26 15:40:23 +0800

    ✨ feat: 添加avaliability接口

--- app.py
+++ app.py
@@ -1,27 +1,44 @@
 import json
-
 import requests
 from flask import jsonify, make_response, Flask, request
-
-from config import Config
+from jwt_config import JwtConfig
 
 app = Flask(__name__)
-config = Config()
+config = JwtConfig()
 
-@app.route('/api/v1/weather/<string:language>/<float:latitude>/<float:longitude>')
-def weatherLoc(language, latitude, longitude):
-    dailyurl = f"https://weatherkit.apple.com/api/v1/weather/{language}/{latitude}/{longitude}"
+
+# 获取指定位置的天气数据 https://developer.apple.com/documentation/weatherkitrestapi/get_api_v1_weather_language_latitude_longitude
+@app.route('/api/v1/weather/<string:language>/<float:latitude>/<float:longitude>', methods=['GET'])
+def weatherLoc(language: str, latitude: float, longitude: float):
+    url = f"https://weatherkit.apple.com/api/v1/weather/{language}/{latitude}/{longitude}"
     dataSets = request.args.get("dataSets")
     timezone = request.args.get("timezone")
     contryCode = request.args.get("countryCode")
+
     param = {
-        "dataSets":dataSets,
-        "timezone":timezone,
-        "countryCode":contryCode
+        "dataSets": dataSets,
+        "timezone": timezone,
+        "countryCode": contryCode
     }
-    res = requests.get(dailyurl, headers={"Authorization": ("Bearer " + config.genToken())},params=param)
+    return __requestAndLoadJson(url, param)
+
+
+# 确定可用于指定位置的数据集 https://developer.apple.com/documentation/weatherkitrestapi/get_api_v1_availability_latitude_longitude
+@app.route("/api/v1/availability/<float:latitude>/<float:longitude>", methods=['GET'])
+def availability(latitude: float, longitude: float):
+    url = f"https://weatherkit.apple.com/api/v1/availability/{latitude}/{longitude}"
+    country = request.args.get("country")
+    param = {
+        "country": country
+    }
+    return __requestAndLoadJson(url, param)
+
+
+def __requestAndLoadJson(url, param: dict):
+    res = requests.get(url, headers={"Authorization": ("Bearer " + config.genToken())}, params=param)
     res = json.loads(res.content)
-    return make_response(jsonify(res))
+    res = make_response(jsonify(res))
+    return res
 
 
 if __name__ == '__main__':
